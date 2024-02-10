271fe674908a4d5615d5bbf22a335e00ee0ce920
Bohdan Bichuk <bichuk1234.kk@gmail.com>
2024-02-10 18:03:58 +0200

    add navigation, router transition, home page, particles, header, about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
-import Layout from "../components/Layout";
+import '../styles/components.css';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-  );
+import Layout from "../components/Layout";
+import {useRouter} from "next/router";
+import {AnimatePresence, motion} from "framer-motion";
+import Transition from "../components/Transition";
+
+
+function MyApp({Component, pageProps}) {
+    const router = useRouter()
+    return (
+        <Layout>
+            <AnimatePresence mode='wait'>
+                <motion.div key={router.route} className='h-full'>
+                    <Transition />
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
+        </Layout>
+    );
 }
 
 export default MyApp;
